87d39525e47f61c6632e6739a0c867d8035652c3
Bishal Sarker <mr.bishalsarker@gmail.com>
2021-04-14 14:59:46 +0600

    commands repository added

---
+++ BComm.PM.Repositories/Common/CommandsRepository.cs
@@ -0,0 +1,38 @@
+﻿using BComm.PM.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BComm.PM.Repositories.Common
+{
+    public class CommandsRepository<T> where T : BaseEntity
+    {
+        public async Task Add(T entity)
+        {
+            using(var context = new MainDbContext())
+            {
+                await context.AddAsync<T>(entity);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task Update(T entity)
+        {
+            using (var context = new MainDbContext())
+            {
+                context.Update<T>(entity);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task Delete(T entity)
+        {
+            using (var context = new MainDbContext())
+            {
+                context.Remove<T>(entity);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
