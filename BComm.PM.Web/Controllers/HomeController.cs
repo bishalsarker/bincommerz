caad965be7166701976cdce2fd0396056184bcc5
Bishal Sarker <mr.bishalsarker@gmail.com>
2021-06-13 01:38:42 +0600

    default portal route added

---
+++ BComm.PM.Web/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BComm.PM.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly string _hostURL;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _hostURL = configuration.GetSection("HostURL").Value;
+        }
+
+        public IActionResult Index()
+        {
+            return Redirect(_hostURL + "retail-admin/portal");
+        }
+    }
+}
