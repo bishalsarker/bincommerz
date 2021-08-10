943e8d116daf0f7fec438e150b0d93f09048015b
Bishal Sarker <mr.bishalsarker@gmail.com>
2021-08-10 12:52:56 +0600

    cron endpoint added

---
+++ BComm.PM.Web/Controllers/MiscellaneousController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BComm.PM.Web.Controllers
+{
+    [Route("miscellaneous")]
+    [ApiController]
+    public class MiscellaneousController : ControllerBase
+    {
+        [HttpGet("awake")]
+        public IActionResult Awake()
+        {
+            return Ok("I'm awake now...");
+        }
+    }
+}
